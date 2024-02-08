1d2ed7154ee8af1ecea3ed1d503ca578ffd76f69
SeKo <kochnev_serezhenka14@mail.ru>
2024-02-08 18:38:33 +0500

    refactor 2

--- reactor/src/components/Sidebar.tsx
+++ reactor/src/components/Sidebar.tsx
@@ -1,9 +1,9 @@
 import {Flex,Image, Heading,Link} from '@chakra-ui/react';
-import avatar from '../assets/image/avatar.svg'
-import list from '../assets/image/list.svg'
-import plan from '../assets/image/plan.svg'
-import nomenclature from '../assets/image/nomenclature.svg'
-import setting from '../assets/image/setting.svg'
+import avatar from '../assets/image/avatar.png'
+import list from '../assets/icons/list.svg'
+import plan from '../assets/icons/plan.svg'
+import nomenclature from '../assets/icons/nomenclature.svg'
+import setting from '../assets/icons/setting.svg'
 
 type Props={
 	isAdmin: boolean
