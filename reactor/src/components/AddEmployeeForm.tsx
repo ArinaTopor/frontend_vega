508bc4b14b028e3d0db39dd3f3b4e926b5d5c149
Arina Topor <arinatopor695@gmail.com>
2024-02-09 14:27:29 +0500

    rename files and delete unused files

--- reactor/src/components/AddEmployeeForm.tsx
+++ reactor/src/components/AddEmployeeForm.tsx
@@ -1,41 +1,53 @@
-import FillingInput from './CustomInput/FormInput'
-import FillingSelect from './AddEmployeeSelect'
-type Props={
-	isAdmin:boolean
-}
+import FormInput from './custom-input/FormInput';
+import Select from './CustomSelect';
+type Props = {
+    isAdmin: boolean;
+};
 
-const roles=[
-	'role1',
-	'role2',
-	'role3'
-]
+const roles = ['role1', 'role2', 'role3'];
 
-const sites=[
-	'site1',
-	'site2',
-	'site3'
-]
+const sites = ['site1', 'site2', 'site3'];
 
-const AddEmployeeForm=({isAdmin}:Props)=>{
+const AddEmployeeForm = ({ isAdmin }: Props) => {
+    return (
+        <form>
+            <FormInput name='name' required={true} type='text' label='ФИО' />
+            <FormInput name='login' required={true} type='text' label='Логин' />
+            {isAdmin ? (
+                <>
+                    <FormInput
+                        name='password'
+                        required={true}
+                        type='password'
+                        label='Пароль для входа'
+                    />
+                </>
+            ) : (
+                <>
+                    <Select
+                        name='role'
+                        placeholder=' '
+                        required={true}
+                        options={roles}
+                        label='Роль'
+                    />
+                    <Select
+                        name='site'
+                        placeholder=' '
+                        required={true}
+                        options={sites}
+                        label='Участок'
+                    />
+                    <FormInput
+                        name='password'
+                        required={true}
+                        type='text'
+                        label='Пароль'
+                    />
+                </>
+            )}
+        </form>
+    );
+};
 
-	return(
-		<form>
-			<FillingInput name='name' required={true} type='text' label='ФИО'/>
-			<FillingInput name='login' required={true} type='text' label='Логин'/>
-			{
-				isAdmin ? 
-				<>
-				<FillingInput name='password' required={true} type='password' label='Пароль для входа'/>
-				</>:
-				<>
-				<FillingSelect name='role' placeholder=' ' required={true} 
-				options={roles} label='Роль'/>
-				<FillingSelect name='site' placeholder=' ' required={true} options={sites} label='Участок'/>
-				<FillingInput name='password' required={true} type='text' label='Пароль'/>
-				</>
-			}
-		</form>
-	)
-}
-
-export default AddEmployeeForm
+export default AddEmployeeForm;
